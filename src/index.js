460982e0cf4d272ba4fc6940792622c492233f58
patelotech <pj.patriciojulian@gmail.com>
2019-01-22 11:18:29 +0000

    lint

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 // If you export a function...
 exports.yourFunction = function functionName() {
-    // to do ... //
+  // to do ... //
 };
 
 
 // If you export a class...
 class classToExport {
-      constructor(name) {
-    this.attribute1 = "";
-    this.attribute2 = "";
-  } 
-  
-  classFunction1 () {}
-  classFunction2 () {}
-  classFunction3 () {}
-  
-};
+  constructor(name) {
+    this.attribute1 = '';
+    this.attribute2 = '';
+  }
+
+  classFunction1() {}
+
+  classFunction2() {}
+
+  classFunction3() {}
+}
 
 module.exports = classToExport;
-
