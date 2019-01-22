df30d55b89c371fdc08b442395fb755aedfc86e1
patelotech <pj.patriciojulian@gmail.com>
2019-01-22 11:41:36 +0000

    add comments

--- src/index.js
+++ src/index.js
@@ -5,16 +5,39 @@ exports.yourFunction = function functionName() {
 
 
 // If you export a class...
+
+/**
+ * Explain class.
+ * @example
+ * new classToExport(params)
+ */
 class classToExport {
+    
   constructor(name) {
     this.attribute1 = '';
     this.attribute2 = '';
   }
 
+  /**
+   * classFunction1
+   * @param {} - Explain param
+   * @param {} - Explain param
+   * @return {} - Explain what the function returns...
+   */
   classFunction1() {}
-
+  /**
+   * classFunction2
+   * @param {} - Explain param
+   * @param {} - Explain param
+   * @return {} - Explain what the function returns...
+   */
   classFunction2() {}
-
+  /**
+   * classFunction3
+   * @param {} - Explain param
+   * @param {} - Explain param
+   * @return {} - Explain what the function returns...
+   */
   classFunction3() {}
 }
 
